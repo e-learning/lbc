bb3caea22d65f756d57e0e8ca878ec947d42e09c
Lapteva Yulya <yulyalapteva@yandex.ru>
2011-11-28 22:51:04 +0000

    server login request added

--- src/ru/leti/lapteva/LbcActivity.java
+++ src/ru/leti/lapteva/LbcActivity.java
@@ -1,5 +1,13 @@
 package ru.leti.lapteva;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
@@ -9,17 +17,17 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
-//TODO: create class-wrapper for LocationService with notification API
-
 public class LbcActivity extends Activity
 implements LocationListener {
-	/** Called when the activity is first created. */
-	
 	private TextView tv;
 	private Application app;
 	private Context con;
 	private LocationManager lm;
 	private Location loc;
+	
+	static String loginrequest = "{\"login\":\"Paul\",\"password\":\"test\"}";
+	static String url = "http://tracks.osll.spb.ru:81/";
+	static String login = "service/login";
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,20 +35,62 @@ implements LocationListener {
         tv = new TextView(this);
         app = getApplication();
         con = app.getApplicationContext();
-        lm = (LocationManager) con.getSystemService(Context.LOCATION_SERVICE);
+        GPSInit();
+        
+        tv.setText(tv.getText() + doLoginRequest());
+
+        setContentView(tv);
+    }
+    
+    private void GPSInit() {
+    	lm = (LocationManager) con.getSystemService(Context.LOCATION_SERVICE);
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (loc != null)
-        	tv.setText(loc.toString());
-        else
-        	tv.setText("unlonown");
-        setContentView(tv);
+    }
+    
+    private String doLoginRequest() {
+    	return doRequest(url + login, loginrequest);
+    }
+    
+    private String doRequest(String url, String query) {
+    	String response = null;
+    	try {
+    		URL svr = new URL(url);
+    		HttpURLConnection con = (HttpURLConnection) svr.openConnection();
+    		con.setRequestMethod("POST");
+    		con.setDoOutput(true);
+    		con.setDoInput(true);
+    		con.connect();
+    		
+    		OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
+    		writer.write(query);
+    		writer.close();
+    		if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+    			InputStream is = con.getInputStream();
+    			InputStreamReader reader = new InputStreamReader(is);
+    			StringBuffer data = new StringBuffer();
+    			int c;
+    			while ((c = reader.read()) != -1)
+    				data.append((char) c);   		
+    			response = data.toString();
+    		} else {
+    			response = "Server does not response";
+    		}
+		} catch (MalformedURLException e) {
+			response = "wrong url: " + e.getMessage();
+		} catch (IOException e) {
+			response = "IOException: " + e.getMessage();
+    	} catch (Exception e) {	
+    		response = "other: " + e.getMessage();
+    	}
+    	
+    	return response;
     }
 
 	public void onLocationChanged(Location arg0) {
 		loc = arg0;
 		if (loc != null)
-			tv.setText(loc.toString());
+			tv.setText(tv.getText() + loc.toString());
 	}
 
 	public void onProviderDisabled(String provider) {
@@ -50,6 +100,5 @@ implements LocationListener {
 	}
 
 	public void onStatusChanged(String provider, int status, Bundle extras) {	
-		//nothing TODO
 	}
 }
