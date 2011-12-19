03cef0cbd09405188e988984cd8712e77fd919fa
Lapteva Yulya <yulyalapteva@yandex.ru>
2011-12-19 21:54:53 +0000

    GPS Location with UI

--- gen/ru/lapteva/geochat/R.java
+++ gen/ru/lapteva/geochat/R.java
@@ -18,12 +18,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int line=0x7f060006;
+        public static final int line=0x7f060009;
         public static final int linearLayout1=0x7f060000;
         public static final int linearLayout2=0x7f060001;
-        public static final int linearLayout3=0x7f060005;
-        public static final int messages=0x7f060004;
-        public static final int send=0x7f060007;
+        public static final int linearLayout3=0x7f060008;
+        public static final int linearLayout4=0x7f060004;
+        public static final int messages=0x7f060007;
+        public static final int radiusEdit=0x7f060006;
+        public static final int send=0x7f06000a;
+        public static final int textView1=0x7f060005;
         public static final int user=0x7f060003;
         public static final int userLabel=0x7f060002;
     }
@@ -34,6 +37,7 @@ public final class R {
         public static final int app_name=0x7f040001;
         public static final int empty=0x7f040002;
         public static final int login=0x7f040000;
+        public static final int radius=0x7f040004;
         public static final int send=0x7f040003;
     }
 }
--- src/ru/lapteva/geochat/ChatActivity.java
+++ src/ru/lapteva/geochat/ChatActivity.java
@@ -1,16 +1,23 @@
 package ru.lapteva.geochat;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ChatActivity extends Activity {
-	private EditText user;
-	private EditText line;
-	private TextView messages;
+public class ChatActivity extends Activity
+implements LocationListener {
+	private EditText user = null;           //user name поле
+	private EditText line = null;           //поле ввода сообщения
+	private TextView messages = null;       //поле сообщений
+	private EditText radius = null;         //радиус действия чата
+	private Location loc = null;            //координаты
+	private LocationManager locManager = null;
 	
     /** Called when the activity is first created. */
     @Override
@@ -21,12 +28,17 @@ public class ChatActivity extends Activity {
         user = (EditText) findViewById(R.id.user);
         line = (EditText) findViewById(R.id.line);
         messages = (TextView) findViewById(R.id.messages);
+        radius = (EditText) findViewById(R.id.radiusEdit);
         
-        initGPS();
+        initGPS(); //инициализация Location Service
     }
     
     private void initGPS() {
-    	
+    	locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    	loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    	if (loc == null)
+    		loc = locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+    	locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
     }
     
     public void onSendClick(View view) {
@@ -34,7 +46,7 @@ public class ChatActivity extends Activity {
     	case R.id.send:
     		String name = user.getText().toString();
     		if (name.trim().equals("")) {
-    			Toast.makeText(this, "Please enter user name... Дурень.", Toast.LENGTH_LONG).show();
+    			Toast.makeText(this, "Please enter user name...", Toast.LENGTH_LONG).show();
     			return;
     		}
     		appendText(name + ": " + line.getText() + "\n");
@@ -46,4 +58,28 @@ public class ChatActivity extends Activity {
     private void appendText(String mes) {
     	messages.append(mes);
     }
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		loc = arg0;
+		if (loc != null) {
+			appendText("New coordinates: " + loc.getLatitude() 
+					+ "," + loc.getLongitude());
+		}
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// nothing to do
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// nothing to do
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// nothing to do
+	}
 }
